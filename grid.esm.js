8b1f112253a664397462682f15dc42dcd6466ac4
turtlefingers <turtlefingers00@gmail.com>
2024-06-21 21:03:24 +0900

    module

--- grid.esm.js
+++ grid.esm.js
@@ -1,4 +1,4 @@
-const grid = (callback=()=>{},gap=50) => {
+export const grid = (callback=()=>{},gap=50) => {
   for(let y=0; y<=height; y+=gap){
     for(let x=0; x<=width; x+=gap){
       callback(x,y);
