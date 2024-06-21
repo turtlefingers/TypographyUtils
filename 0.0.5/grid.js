5b6efe98345e5dc2b8522a6ea94164d918519a06
turtlefingers <turtlefingers00@gmail.com>
2024-06-21 21:42:37 +0900

    fix triangle grid to equilateral triangle

--- 0.0.5/grid.js
+++ 0.0.5/grid.js
@@ -7,14 +7,17 @@ const grid = (callback=()=>{},gap=50) => {
 }
 
 const gridTriangle = (callback = () => {}, gap = 50) => {
-  for (let y = 0; y <height; y += gap) {
-    for (let x = 0; x <width; x += gap) {
+  const verticalGap = Math.sqrt(3) / 2 * gap;
+  
+  for (let y = 0; y <= height; y += verticalGap) {
+    for (let x = 0; x <= width; x += gap) {
       // 짝수 줄과 홀수 줄의 X 좌표 오프셋
-      const offsetX = (y / gap) % 2 === 0 ? 0 : gap / 2;
+      const offsetX = (Math.floor(y / verticalGap) % 2 === 0) ? 0 : gap / 2;
       callback(x + offsetX, y);
     }
   }
 };
+
 
 const chain = (callback=()=>{},mouseX,mouseY,arr,gap=50) => {
   if(arr.length==0)callback(mouseX,mouseY);
