5cfac6994559a2dfbf03e7f8558937dcfb815505
turtlefingers <turtlefingers00@gmail.com>
2024-06-21 21:12:43 +0900

    range to under width/height

--- 0.0.2/grid.js
+++ 0.0.2/grid.js
@@ -1,14 +1,14 @@
 const grid = (callback=()=>{},gap=50) => {
-  for(let y=0; y<=height; y+=gap){
-    for(let x=0; x<=width; x+=gap){
+  for(let y=0; y<height; y+=gap){
+    for(let x=0; x<width; x+=gap){
       callback(x,y);
     }
   }
 }
 
 const gridTriangle = (callback = () => {}, gap = 50) => {
-  for (let y = 0; y <= height; y += gap) {
-    for (let x = 0; x <= width; x += gap) {
+  for (let y = 0; y <height; y += gap) {
+    for (let x = 0; x <width; x += gap) {
       // 짝수 줄과 홀수 줄의 X 좌표 오프셋
       const offsetX = (y / gap) % 2 === 0 ? 0 : gap / 2;
       callback(x + offsetX, y);
